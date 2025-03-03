b6df3eccee31131dfb9b966e120ab63293db245e
tallpizza <tallpizza@naver.com>
2025-03-04 00:11:16 +0900

    format

--- components/main/organisms/TypedIntroduceWrapper.tsx
+++ components/main/organisms/TypedIntroduceWrapper.tsx
@@ -38,7 +38,7 @@ export function TypedIntroduceWrapper() {
   return (
     <div className="min-h-42 py-4">
       <span
-        className="text-xl leading-7 text-gray-500 dark:text-gray-300 emoji"
+        className="emoji text-xl leading-7 text-gray-500 dark:text-gray-300"
         ref={typedRef}
       ></span>
     </div>
